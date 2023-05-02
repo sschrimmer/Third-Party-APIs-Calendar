df5ca8145f2405935c32b2c29c11a70d19b68a64
Steven <stevenschrimmer1@gmail.com>
2023-05-01 17:49:24 -0700

    redme file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# Third-Party-APIs-Calendar
+#Third Party API Calendar
+
+
+## Description
+
+This is a daily calendar to help manage tasks by the hour 
+
+[link to project] https://sschrimmer.github.io/Third-Party-APIs-Calendar/
+
+![](/Images/sschrimmer.github.io_Third-Party-APIs-Calendar_(iPad%20Mini).png)
+
+## Installation
+
+N/A
+
+## Usage
+
+Use this app to help schedule your day and tasks
+
+
+## Credits
+
+N/A
+
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var currentDate = moment(). format('dddd') + " " + moment().format("Do MMM YYYY");
-  var currentHour = moment().format('h:mm:ss a');
+  var currentDate =
+    moment().format("dddd") + " " + moment().format("Do MMM YYYY");
+  var currentHour = moment().format("h:mm:ss a");
 
   var nineAm = $("#9");
   var tenAm = $("#10");
@@ -14,89 +15,89 @@ $(function () {
   var threePm = $("#15");
   var fourPm = $("#16");
   var fivePm = $("#17");
-  var sixPm= $("#18");
+  var sixPm = $("#18");
   var sevenPm = $("#19");
-  
+
   var hour = moment().hours();
   var userInput;
   var hourSpan;
-  
-  var interval = setInterval(function() {
-  var momentNow = moment();
-  $('currentDay').html(momentNow.format('YYYY MMMM DD') + '  '
-  + momentNow.format('dddd')
-  .substring(0,3).toUpperCase());
-  $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
-}, 100);
 
-function initPage() {
-  console.log("Current Hour" + hour);
-  var init9 = JSON.parse(localStorage.getItem("09:00 am"));
-  nineAm.val(init9);
+  var interval = setInterval(function () {
+    var momentNow = moment();
+    $("currentDay").html(
+      momentNow.format("YYYY MMMM DD") +
+        "  " +
+        momentNow.format("dddd").substring(0, 3).toUpperCase()
+    );
+    $("#currentDay").html(currentDate + " " + momentNow.format("hh:mm:ss A"));
+  }, 100);
 
-  var init10 = JSON.parse(localStorage.getItem("10:00 am"));
-  tenAm.val(init10);
+  function initPage() {
+    console.log("Current Hour" + hour);
+    var init9 = JSON.parse(localStorage.getItem("09:00 am"));
+    nineAm.val(init9);
 
-  var init11 = JSON.parse(localStorage.getItem("11:00 am"));
-  elevenAm.val(init11);
+    var init10 = JSON.parse(localStorage.getItem("10:00 am"));
+    tenAm.val(init10);
 
-  var init12 = JSON.parse(localStorage.getItem("12:00 pm"));
-  twelvePm.val(init12);
+    var init11 = JSON.parse(localStorage.getItem("11:00 am"));
+    elevenAm.val(init11);
 
-  var init1 = JSON.parse(localStorage.getItem("01:00 pm"));
-  onePm.val(init1);
+    var init12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    twelvePm.val(init12);
 
-  var init2 = JSON.parse(localStorage.getItem("02:00 pm"));
-  twoPm.val(init2);
+    var init1 = JSON.parse(localStorage.getItem("01:00 pm"));
+    onePm.val(init1);
 
-  var init3 = JSON.parse(localStorage.getItem("03:00 pm"));
-  threePm.val(init3);
+    var init2 = JSON.parse(localStorage.getItem("02:00 pm"));
+    twoPm.val(init2);
 
-  var init4 = JSON.parse(localStorage.getItem("04:00 pm"));
-  fourPm.val(init4);
+    var init3 = JSON.parse(localStorage.getItem("03:00 pm"));
+    threePm.val(init3);
 
-  var init5 = JSON.parse(localStorage.getItem("05:00 pm"));
-  fivePm.val(init5);
+    var init4 = JSON.parse(localStorage.getItem("04:00 pm"));
+    fourPm.val(init4);
 
-  var init6 = JSON.parse(localStorage.getItem("06:00 pm"));
-  sixPm.val(init6);
+    var init5 = JSON.parse(localStorage.getItem("05:00 pm"));
+    fivePm.val(init5);
 
-  var init7 = JSON.parse(localStorage.getItem("07:00 pm"));
-  sevenPm.val(init7);
-}
+    var init6 = JSON.parse(localStorage.getItem("06:00 pm"));
+    sixPm.val(init6);
 
-function background () {
-  $(".form-control").each(function () {
-    var timeTest = parseInt($(this).attr("id"));
-    // hour = parseInt(hour); 
-    console.log ("hourBlock", timeTest, typeof timeTest);
-    console.log("currentHour", hour, typeof hour);
+    var init7 = JSON.parse(localStorage.getItem("07:00 pm"));
+    sevenPm.val(init7);
+  }
 
-    if (hour > timeTest) {
-      $(this).addClass("past");
-} else if (hour < timeTest) {
-  $(this).addClass("future");
-} else {
-  $(this).addClass("present");
-}
+  function background() {
+    $(".form-control").each(function () {
+      var timeTest = parseInt($(this).attr("id"));
+      // hour = parseInt(hour);
+      console.log("hourBlock", timeTest, typeof timeTest);
+      console.log("currentHour", hour, typeof hour);
+
+      if (hour > timeTest) {
+        $(this).addClass("past");
+      } else if (hour < timeTest) {
+        $(this).addClass("future");
+      } else {
+        $(this).addClass("present");
+      }
+    });
+  }
+  $(document).ready(function () {
+    initPage();
+    background();
+
+    $(".saveBtn").on("click", function () {
+      userInput = $(this).siblings(".form-control").val().trim();
+      console.log(userInput);
+      hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+      console.log(hourSpan);
+      localStorage.setItem(hourSpan, JSON.stringify(userInput));
+    });
+    $("#clearDay").on("click", function () {
+      localStorage.clear();
+      initPage();
+    });
   });
-}
-$(document).ready(function() {
-
-  initPage()
-  background()
-
-  $(".saveBtn").on("click", function(){
-    userInput = $(this).siblings(".form-control").val().trim();
-    console.log(userInput);
-    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-    console.log(hourSpan);
-    localStorage.setItem(hourSpan, JSON.stringify(userInput));
-
-  })
-  $("#clearDay").on("click", function(){
-    localStorage.clear();
-    initPage()
-  })
-})
-})
+});
